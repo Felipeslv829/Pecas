9365ac2343edb12937c60e1eba8ef1b33adfc110
Felipe <Felipe@DESKTOP-FGPDRFG>
2019-12-08 20:30:29 -0300

    Conexão com MySQL

--- pecas/src/main/java/com/pecas/models/produto.java
+++ pecas/src/main/java/com/pecas/models/produto.java
@@ -1,9 +1,28 @@
 package com.pecas.models;
 
-public class produto {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class produto implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private long codigo;
 	private String nome;
 	private int valor;
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 	private int referencia;
 	public String getNome() {
 		return nome;
